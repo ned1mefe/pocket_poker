b8f61609a43f5cdfcac1281ceb38d7843795ea71
Nedim Efe Candas <nedimefe.candas@gmail.com>
2024-03-16 14:50:54 +0300

    make main menu template

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    public void PlayButtonPressed()
+    {
+        // should open config page
+    }
+
+    public void HtpButtonPressed()
+    {
+        // should open a window to explain
+    }
+}
