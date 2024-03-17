7ed935e3b518f959c9c9c70a8fe1976b5f8a6377
Nedim Efe Candas <nedimefe.candas@gmail.com>
2024-03-17 15:26:47 +0300

    add config screen

---
+++ Assets/Scripts/GameConfig.cs
@@ -0,0 +1,9 @@
+public class GameConfig
+{
+    public readonly short PlayerCount;
+    
+    public GameConfig(short playerCount)
+    {
+        PlayerCount = playerCount;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,17 +1,42 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
 
 public class UIManager : MonoBehaviour
 {
-    public static UIManager Instance;
+    // public static UIManager Instance; not needed now
+    private GameObject _mainMenuScreen;
+    private GameObject _configScreen;
+    private TextMeshProUGUI _playerCountText;
+    private Slider _playerCountSlider;
+
+    private void Awake()
+    {
+        _mainMenuScreen = GameObject.Find("Canvas").transform.Find("MainMenuScreen").gameObject;
+        _configScreen = GameObject.Find("Canvas").transform.Find("ConfigScreen").gameObject;
+        _playerCountText   = _configScreen.transform.Find("PlayerCount").GetComponentInChildren<TextMeshProUGUI>();
+        _playerCountSlider = _configScreen.transform.Find("PlayerCount").GetComponentInChildren<Slider>();
+        _playerCountSlider.onValueChanged.AddListener(PlayerCountUpdate);
+    }
 
     public void PlayButtonPressed()
     {
-        // should open config page
+        _mainMenuScreen.gameObject.SetActive(false);
+        _configScreen.gameObject.SetActive(true);
     }
 
+    private void PlayerCountUpdate(float value)
+    {
+        _playerCountText.text = "Player Count: " + value;
+    }
     public void HtpButtonPressed()
     {
         // should open a window to explain
+    }
+
+    public void StartButtonPressed()
+    {
+        GameConfig config = new GameConfig((short)_playerCountSlider.value);
+        // should change the scene
     }
 }
